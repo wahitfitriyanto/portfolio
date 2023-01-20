31c6bb1dcf45bfc99ad01121fc5ae202b6c51b2b
Wahit Fitriyanto <26300406+wahitfitriyanto@users.noreply.github.com>
2023-01-20 14:43:44 +0000

    update portfolio

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,29 +31,29 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "The Supreme Syndicate",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Worked as a server developer and as a server operator since 2021.",
+    url: "#ThisProjecthasended",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Central West Java",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Im one of the founders and developers of the Central West Java server based on the SA-MP game server since 2020 and ended in 2021.",
+    url: "https://github.com/wahitfitriyanto/Central-West-Java",
   },
   {
-    title: "My Resume Site",
+    title: "Custom OpenWrt",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Created from official openwrt repos and compiled to my router. Includes my experience and linux abilities.",
+    url: "https://github.com/wahitfitriyanto/OpenWrt",
   },
-  {
+  /*{
     title: "GitHub Codespaces and github.dev",
     description:
       "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
     url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
-  },
+  },*/
 ];
 
 const Portfolio = () => {
